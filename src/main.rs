bc0d9dd7890447bd3d5f9820f88a6c2d64dc9f9b
chainyo <t.chaigneau.tc@gmail.com>
2023-04-07 09:13:30 +0200

    add list of tasks retrieval

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
+
+    struct Task {
+        name: String,
+        done: bool,
+    }
+
     loop {
         println!("To-Do List Menu:");
         println!("1. Add a task");
@@ -9,9 +15,9 @@ fn main() {
         println!("6. Load tasks from a file");
         println!("7. Exit");
 
-        let mut input = String::new();
+        let mut input: String = String::new();
         std::io::stdin().read_line(&mut input).expect("Failed to read input");
-        let input = input.trim();
+        let input: &str = input.trim();
 
         // Attempt to parse the input as a u8, if it fails, print an error message and continue
         let choice = match input.parse::<u8>() {
@@ -22,14 +28,27 @@ fn main() {
             },
         };
 
+        let mut tasks: Vec<Task> = Vec::new();
+
         match choice {
             1 => {
                 println!("Enter a task:");
-                let mut task = String::new();
-                std::io::stdin().read_line(&mut task).expect("Failed to read input");
-                println!("Added task: {}", task);
+                let mut task_text: String = String::new();
+                std::io::stdin().read_line(&mut task_text).expect("Failed to read input");
+                let task_text = task_text.trim().to_string();
+                let task = Task {
+                    name: task_text,
+                    done: false,
+                };
+                tasks.push(task);
+                println!("Added task: {}", tasks.last().unwrap().name);
             },
-            2 => println!("List all tasks"),
+            2 => {
+                println!("Tasks:");
+                for (index, task) in tasks.iter().enumerate() {
+                    println!("{}. {} [{}]", index + 1, task.name, if task.done { "✅" } else { "⭕" });
+                };
+            }
             3 => println!("Mark a task as done"),
             4 => println!("Delete a task"),
             5 => println!("Save tasks to a file"),
